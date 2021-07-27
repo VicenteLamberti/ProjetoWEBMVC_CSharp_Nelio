168667d2dfcc35a44664330da12b67e90c75ab94
vicente_leonardo <vicente@geodados.com.br>
2021-07-27 12:20:46 -0300

    Criação da controller departaments por scalfolding

---
+++ ProjetoWEBMVC_CSharp_Nelio/Data/ProjetoWEBMVC_CSharp_NelioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWEBMVC_CSharp_Nelio.Models;
+
+namespace ProjetoWEBMVC_CSharp_Nelio.Data
+{
+    public class ProjetoWEBMVC_CSharp_NelioContext : DbContext
+    {
+        public ProjetoWEBMVC_CSharp_NelioContext (DbContextOptions<ProjetoWEBMVC_CSharp_NelioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoWEBMVC_CSharp_Nelio.Models.Departament> Departament { get; set; }
+    }
+}
--- ProjetoWEBMVC_CSharp_Nelio/Startup.cs
+++ ProjetoWEBMVC_CSharp_Nelio/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWEBMVC_CSharp_Nelio.Data;
 
 namespace ProjetoWEBMVC_CSharp_Nelio
 {
@@ -33,6 +35,9 @@ namespace ProjetoWEBMVC_CSharp_Nelio
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoWEBMVC_CSharp_NelioContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoWEBMVC_CSharp_NelioContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
