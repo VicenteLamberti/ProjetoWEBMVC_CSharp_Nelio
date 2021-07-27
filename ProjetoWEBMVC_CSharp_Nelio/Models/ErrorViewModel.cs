0e10f00328dcb61bf02a9ac2b4ed3b35b4cb01d0
vicente_leonardo <vicente@geodados.com.br>
2021-07-27 09:46:11 -0300

    Add project files.

---
+++ ProjetoWEBMVC_CSharp_Nelio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoWEBMVC_CSharp_Nelio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
