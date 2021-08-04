6f53cb3e939abdb45b59eea050b08d2a92b4f42a
vicente_leonardo <vicente@geodados.com.br>
2021-08-04 07:57:04 -0300

    View de Error

--- ProjetoWEBMVC_CSharp_Nelio/Models/ViewModels/ErrorViewModel.cs
+++ ProjetoWEBMVC_CSharp_Nelio/Models/ViewModels/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace ProjetoWEBMVC_CSharp_Nelio.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        public string Message { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
