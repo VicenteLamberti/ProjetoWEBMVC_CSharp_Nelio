f97af9ff9afe623f45594ded43b425ead49c7c69
vicente_leonardo <vicente@geodados.com.br>
2021-08-04 07:33:16 -0300

    Details, e eager loading

--- ProjetoWEBMVC_CSharp_Nelio/Services/SellerService.cs
+++ ProjetoWEBMVC_CSharp_Nelio/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoWEBMVC_CSharp_Nelio.Services
 {
@@ -29,7 +30,7 @@ namespace ProjetoWEBMVC_CSharp_Nelio.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(x => x.Id == id);
+            return _context.Seller.Include(obj => obj.Departament).FirstOrDefault(x => x.Id == id);
         }
         public void Remove(int id)
         {
