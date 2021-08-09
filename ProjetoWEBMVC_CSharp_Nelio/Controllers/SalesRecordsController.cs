218c3182f0cbbd74380dc7c85afae674ec29626e
vicente_leonardo <vicente@geodados.com.br>
2021-08-09 08:11:31 -0300

    Simple search

--- ProjetoWEBMVC_CSharp_Nelio/Controllers/SalesRecordsController.cs
+++ ProjetoWEBMVC_CSharp_Nelio/Controllers/SalesRecordsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoWEBMVC_CSharp_Nelio.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,31 @@ namespace ProjetoWEBMVC_CSharp_Nelio.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordsService _salesRecordsService;
+
+        public SalesRecordsController(SalesRecordsService salesRecordsService)
+        {
+            _salesRecordsService = salesRecordsService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if(minDate.HasValue != true)
+            {
+                minDate = new DateTime(2018, 1, 1);
+            }
+            if(maxDate.HasValue != true)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordsService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
         public IActionResult GroupingSearch()
         {
---
+++ ProjetoWEBMVC_CSharp_Nelio/Services/SalesRecordsService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoWEBMVC_CSharp_Nelio.Data;
+using ProjetoWEBMVC_CSharp_Nelio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoWEBMVC_CSharp_Nelio.Services
+{
+    public class SalesRecordsService
+    {
+        private readonly ProjetoWEBMVC_CSharp_NelioContext _context;
+
+        public SalesRecordsService(ProjetoWEBMVC_CSharp_NelioContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecords>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Departament)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
